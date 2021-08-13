cd985ca989f1a7acf06c9b08b730d56e40c79625
nipuninawodani <61185310+nipuninawodani@users.noreply.github.com>
2021-08-13 21:47:14 +0530

    folders are re-arranged (#18)

--- kmain.c
+++ kmain.c
@@ -1,32 +1,16 @@
-
-/* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
-    }
-
-
-<#include "drivers/frame_buffer.h"
-    #include "drivers/serial_port.h"
-
-#include "frame_buffer.h"
-    #include "serial_port.h"
+ #include "drivers/frame_buffer.h"
+ #include "drivers/serial_port.h"
+ #include "segmentation/memory_segments.h"
 
 
     
     int main(){
 
-           char ptr2[] = "Welcome to CarbonOS";
+           char ptr2[] = "Welcome to XamOS";
    
     
     serial_write(0x3F8, ptr2, 19);
     fb_write(ptr2, 19);
+    segments_install_gdt();
     
-
-
     }
-
-    }
-
-    }
-
